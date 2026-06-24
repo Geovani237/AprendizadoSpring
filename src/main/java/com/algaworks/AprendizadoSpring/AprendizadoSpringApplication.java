6ecb1dcc27815a26d2a3934d6f91b01b90f0d985
Geovani Carlos de Souza <geovani.souza@colaborador.embrapa.br>
2026-06-24 11:00:57 -0300

    Primeiro commit

---
+++ src/main/java/com/algaworks/AprendizadoSpring/AprendizadoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.AprendizadoSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AprendizadoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AprendizadoSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/algaworks/AprendizadoSpring/AprendizadoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algaworks.AprendizadoSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AprendizadoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
