68b6903f60e96c4159d80dedef421d9008794f89
Geovani Carlos de Souza <geovani.souza@colaborador.embrapa.br>
2026-06-30 09:11:39 -0300

    Conhecendo os pontos de injeção e a anotação @Autowired

--- src/main/java/com/algaworks/AprendizadoSpring/di/notificacao/NotificadorEmail.java
+++ src/main/java/com/algaworks/AprendizadoSpring/di/notificacao/NotificadorEmail.java
@@ -3,26 +3,12 @@ package com.algaworks.AprendizadoSpring.di.notificacao;
 import com.algaworks.AprendizadoSpring.di.modelo.Cliente;
 import org.springframework.stereotype.Component;
 
+@Component
 public class NotificadorEmail implements Notificador {
-
-    private boolean caixaAlta;
-    private String hostServidorSmtp;
-
-    public NotificadorEmail(String hostServidorSmtp) {
-        this.hostServidorSmtp = hostServidorSmtp;
-        System.out.println("NotificadorEmail");
-    }
 
     @Override
     public void notificar(Cliente cliente, String mensagem) {
-        if (caixaAlta) {
-            mensagem = mensagem.toUpperCase();
-        }
-        System.out.printf("Notificando %s através do e-mail %s usando SMPT %s: %s\n",
-                cliente.getNome(), cliente.getEmail(), this.hostServidorSmtp, mensagem);
-    }
-
-    public void setCaixaAlta(boolean caixaAlta) {
-        this.caixaAlta = caixaAlta;
+        System.out.printf("Notificando %s através do e-mail %s: %s\n",
+                cliente.getNome(), cliente.getEmail(), mensagem);
     }
 }
--- src/main/java/com/algaworks/AprendizadoSpring/di/service/AtivacaoClienteService.java
+++ src/main/java/com/algaworks/AprendizadoSpring/di/service/AtivacaoClienteService.java
@@ -2,23 +2,36 @@ package com.algaworks.AprendizadoSpring.di.service;
 
 import com.algaworks.AprendizadoSpring.di.modelo.Cliente;
 import com.algaworks.AprendizadoSpring.di.notificacao.Notificador;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-//@Component
+@Component
 public class AtivacaoClienteService {
 
+    //Ponto de injeção no Atributo (Usado pela simplicidade)
+    @Autowired
     private Notificador notificador;
 
-    public AtivacaoClienteService(Notificador notificador) {
-        this.notificador = notificador;
+    //Ponto de injeção no Construtor(Ideal que se use no construtor)
+//    @Autowired
+//    public AtivacaoClienteService(Notificador notificador) {
+//        this.notificador = notificador;
+//    }
 
-        System.out.println("AtivacaoClienteService: " + notificador);
-    }
+//    public AtivacaoClienteService(String qualquer) {
+//
+//    }
 
     public void ativar(Cliente cliente) {
         cliente.ativar();
 
         notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
     }
+
+    //Ponto de injeção no Setter(Menos usado)
+//    @Autowired
+//    public void setNotificador(Notificador notificador) {
+//        this.notificador = notificador;
+//    }
 
 }
