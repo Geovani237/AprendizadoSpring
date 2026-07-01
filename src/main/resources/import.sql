84f303db1da40b511e54b5b406775a4258e110b7
Geovani Carlos de Souza <geovani.souza@colaborador.embrapa.br>
2026-07-01 16:16:24 -0300

    Adicionando Lombok no projeto, e criando as demais classes para Restaurante

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,4 @@
 insert into cozinha (nome) values ('Tailandesa');
 insert into cozinha (nome) values ('Indiana');
+insert into restaurante (nome, taxa_frete) values ('La Ratatouille', '150.00');
+insert into restaurante (nome, taxa_frete) values ('Subway', '20.00');
