814dc2136679a7be3d00e667036b0f2e0edd4bb5
Geovani Carlos de Souza <geovani.souza@colaborador.embrapa.br>
2026-07-01 13:52:36 -0300

    Atualizando um objeto no banco de dados

---
+++ src/main/java/com/algaworks/AprendizadoSpring/jpa/AlterandoCozinhaMain.java
@@ -0,0 +1,26 @@
+package com.algaworks.AprendizadoSpring.jpa;
+
+import com.algaworks.AprendizadoSpring.AprendizadoSpringApplication;
+import com.algaworks.AprendizadoSpring.domain.model.Cozinha;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+import java.util.List;
+
+public class AlterandoCozinhaMain {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new SpringApplicationBuilder(AprendizadoSpringApplication.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
+
+        CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
+
+
+        Cozinha cozinha = new Cozinha();
+        cozinha.setId(1L);
+        cozinha.setNome("Brasileira");
+
+        cadastroCozinha.salvar(cozinha);
+    }
+}
