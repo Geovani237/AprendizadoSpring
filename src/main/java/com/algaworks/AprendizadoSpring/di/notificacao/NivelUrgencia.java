e9aea70f3a684e66b82dc9e5facca4042a3ca236
Geovani Carlos de Souza <geovani.souza@colaborador.embrapa.br>
2026-07-01 09:59:55 -0300

    Iniciando o projeto do curso

--- src/main/java/com/algaworks/AprendizadoSpring/di/notificacao/NivelUrgencia.java
+++
@@ -1,6 +0,0 @@
-package com.algaworks.AprendizadoSpring.di.notificacao;
-
-public enum NivelUrgencia {
-    URGENTE,
-    SEM_URGENCIA
-}
--- src/main/java/com/algaworks/AprendizadoSpring/di/notificacao/Notificador.java
+++
@@ -1,7 +0,0 @@
-package com.algaworks.AprendizadoSpring.di.notificacao;
-
-import com.algaworks.AprendizadoSpring.di.modelo.Cliente;
-
-public interface Notificador {
-    void notificar(Cliente cliente, String mensagem);
-}
