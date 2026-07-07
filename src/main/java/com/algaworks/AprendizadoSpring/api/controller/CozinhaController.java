30b5c9e810c8be8fe556e1feb8e1fcebade210bb
Geovani Carlos de Souza <geovani.souza@colaborador.embrapa.br>
2026-07-07 10:39:26 -0300

    Corrigindo o Status HTTP para resource inexistente

--- src/main/java/com/algaworks/AprendizadoSpring/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/AprendizadoSpring/api/controller/CozinhaController.java
@@ -35,13 +35,11 @@ public class CozinhaController {
     @GetMapping("/{cozinhaId}")
     public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId) {
         Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
-        HttpHeaders headers = new HttpHeaders();
-        headers.add(HttpHeaders.LOCATION, "http://localhost:8080/cozinhas");
-//        return ResponseEntity.status(HttpStatus.OK).body(cozinha);
-//        return ResponseEntity.ok(cozinha);
-        return ResponseEntity
-                .status(HttpStatus.FOUND)
-                .headers(headers)
-                .build();
+
+        if (cozinha != null) {
+            return ResponseEntity.ok(cozinha);
+        }
+
+        return ResponseEntity.notFound().build();
     }
 }
