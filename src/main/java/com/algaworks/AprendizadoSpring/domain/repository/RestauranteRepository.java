95978fe049004eb3712541d4d4182f8528b0eab7
Geovani Carlos de Souza <geovani.souza@colaborador.embrapa.br>
2026-07-15 14:31:07 -0300

    Implementando um repositório SDJ customizado

--- src/main/java/com/algaworks/AprendizadoSpring/domain/repository/RestauranteRepository.java
+++ src/main/java/com/algaworks/AprendizadoSpring/domain/repository/RestauranteRepository.java
@@ -11,7 +11,8 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
+public interface RestauranteRepository
+        extends JpaRepository<Restaurante, Long>, RestauranteRepositoryQueries{
 
     List<Restaurante> queryByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
 
@@ -25,4 +26,5 @@ public interface RestauranteRepository extends JpaRepository<Restaurante, Long> 
     List<Restaurante> findTop2ByNomeContaining(String nome);
 
     int countByCozinhaId(Long cozinhaId);
+
 }
---
+++ src/main/java/com/algaworks/AprendizadoSpring/domain/repository/RestauranteRepositoryQueries.java
@@ -0,0 +1,11 @@
+package com.algaworks.AprendizadoSpring.domain.repository;
+
+import com.algaworks.AprendizadoSpring.domain.model.Restaurante;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public interface RestauranteRepositoryQueries {
+
+    List<Restaurante> find(String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal);
+}
