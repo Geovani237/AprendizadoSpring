264290bb029b273ec7ab771dfe114595ff8ebd84
Geovani Carlos de Souza <geovani.souza@colaborador.embrapa.br>
2026-08-04 10:27:48 -0300

    Simplificando o código com o uso de @ResponseStatus em exceptions

--- src/main/java/com/algaworks/AprendizadoSpring/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/AprendizadoSpring/api/controller/CozinhaController.java
@@ -35,13 +35,15 @@ public class CozinhaController {
 
 
     @GetMapping("/{cozinhaId}")
-    public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId) {
-        Optional<Cozinha> cozinha = cozinhaRepository.findById(cozinhaId);
+    public Cozinha buscar(@PathVariable Long cozinhaId) {
+        return cadastroCozinha.buscarOuFalhar(cozinhaId);
 
-        if (cozinha.isPresent()) {
-            return ResponseEntity.ok(cozinha.get());
-        }
-        return ResponseEntity.notFound().build();
+//        Optional<Cozinha> cozinha = cozinhaRepository.findById(cozinhaId);
+//
+//        if (cozinha.isPresent()) {
+//            return ResponseEntity.ok(cozinha.get());
+//        }
+//        return ResponseEntity.notFound().build();
     }
 
     @PostMapping
@@ -51,32 +53,23 @@ public class CozinhaController {
     }
 
     @PutMapping("/{cozinhaId}")
-    public ResponseEntity<Cozinha> atualizar(@PathVariable Long cozinhaId, @RequestBody Cozinha cozinha) {
-        Optional<Cozinha> cozinhaAtual = cozinhaRepository.findById(cozinhaId);
+    public Cozinha atualizar(@PathVariable Long cozinhaId, @RequestBody Cozinha cozinha) {
+        Cozinha cozinhaAtual = cadastroCozinha.buscarOuFalhar(cozinhaId);
 
-        if (cozinhaAtual.isPresent()) {
-            BeanUtils.copyProperties(cozinha, cozinhaAtual.get(), "id");
+        BeanUtils.copyProperties(cozinha, cozinhaAtual, "id");
+        return cadastroCozinha.salvar(cozinhaAtual);
 
-            Cozinha cozinhaSalva = cadastroCozinha.salvar(cozinhaAtual.get());
-            return ResponseEntity.ok().body(cozinhaSalva);
-        }
-        return ResponseEntity.notFound().build();
+//        Optional<Cozinha> cozinhaAtual = cozinhaRepository.findById(cozinhaId);
+//
+//        if (cozinhaAtual.isPresent()) {
+//            BeanUtils.copyProperties(cozinha, cozinhaAtual.get(), "id");
+//
+//            Cozinha cozinhaSalva = cadastroCozinha.salvar(cozinhaAtual.get());
+//            return ResponseEntity.ok().body(cozinhaSalva);
+//        }
+//        return ResponseEntity.notFound().build();
     }
 
-//    @DeleteMapping("/{cozinhaId}")
-//    public ResponseEntity<Cozinha> remover(@PathVariable Long cozinhaId) {
-//
-//        try {
-//            cadastroCozinha.excluir(cozinhaId);
-//            return ResponseEntity.noContent().build();
-//
-//        } catch (EntidadeNaoEncontradaException e) {
-//            return ResponseEntity.notFound().build();
-//
-//        } catch (EntidadeEmUsoException e) {
-//            return ResponseEntity.status(HttpStatus.CONFLICT).build();
-//        }
-//    }
 
     @DeleteMapping("/{cozinhaId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
--- src/main/java/com/algaworks/AprendizadoSpring/domain/exception/EntidadeNaoEncontradaException.java
+++ src/main/java/com/algaworks/AprendizadoSpring/domain/exception/EntidadeNaoEncontradaException.java
@@ -4,16 +4,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.server.ResponseStatusException;
 
-//@ResponseStatus(value = HttpStatus.NOT_FOUND) //, reason = "Entidade não encontrada")// Se essa excessão foi lançada e não tratada, nos queremos que o status Http seja o especificado por nós
-public class EntidadeNaoEncontradaException extends ResponseStatusException {
+@ResponseStatus(value = HttpStatus.NOT_FOUND)// Se essa excessão foi lançada e não tratada, nos queremos que o status Http seja o especificado por nós
+public class EntidadeNaoEncontradaException extends RuntimeException {
 
     private static final long serialVersionUID = 1L;
 
-    public EntidadeNaoEncontradaException(HttpStatus status, String reason) {
-        super(status, reason);
-    }
-
     public EntidadeNaoEncontradaException(String message) {
-        this(HttpStatus.NOT_FOUND, message);
+        super(message);
     }
 }
