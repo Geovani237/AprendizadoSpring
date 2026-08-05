08fe33e21cd0b99f67cae2f926184932970ff2fe
Geovani Carlos de Souza <geovani.souza@colaborador.embrapa.br>
2026-08-05 10:39:12 -0300

    Tratando exceções globais com @ExceptionHandler e @ControllerAdvice

--- src/main/java/com/algaworks/AprendizadoSpring/api/controller/CidadeController.java
+++ src/main/java/com/algaworks/AprendizadoSpring/api/controller/CidadeController.java
@@ -68,26 +68,26 @@ public class CidadeController {
         cadastroCidade.excluir(cidadeId);
     }
 
-    @ExceptionHandler(EntidadeNaoEncontradaException.class)
-    public ResponseEntity<?> tratarEntidadeNaoEncontradoException(EntidadeNaoEncontradaException e) {
-        Problema problema = Problema.builder()
-                .dataHora(LocalDateTime.now())
-                .mensagem(e.getMessage())
-                .build();
-
-        return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(problema);
-    }
-
-    @ExceptionHandler(NegocioException.class)
-    public ResponseEntity<?> tratarNegocioException(NegocioException e) {
-        Problema problema = Problema.builder()
-                .dataHora(LocalDateTime.now())
-                .mensagem(e.getMessage())
-                .build();
-
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body(problema);
-    }
+//    @ExceptionHandler(EntidadeNaoEncontradaException.class)
+//    public ResponseEntity<?> tratarEntidadeNaoEncontradoException(EntidadeNaoEncontradaException e) {
+//        Problema problema = Problema.builder()
+//                .dataHora(LocalDateTime.now())
+//                .mensagem(e.getMessage())
+//                .build();
+//
+//        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+//                .body(problema);
+//    }
+//
+//    @ExceptionHandler(NegocioException.class)
+//    public ResponseEntity<?> tratarNegocioException(NegocioException e) {
+//        Problema problema = Problema.builder()
+//                .dataHora(LocalDateTime.now())
+//                .mensagem(e.getMessage())
+//                .build();
+//
+//        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+//                .body(problema);
+//    }
 
 }
---
+++ src/main/java/com/algaworks/AprendizadoSpring/api/exceptionhandler/ApiExceptionHandler.java
@@ -0,0 +1,56 @@
+package com.algaworks.AprendizadoSpring.api.exceptionhandler;
+
+import com.algaworks.AprendizadoSpring.domain.exception.EntidadeNaoEncontradaException;
+import com.algaworks.AprendizadoSpring.domain.exception.NegocioException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+
+
+// @ControllerAdvice -> Componente interceptor (AOP) do Spring que centraliza
+// o tratamento de exceções e a formatação de dados em nível de aplicação.
+@ControllerAdvice
+public class ApiExceptionHandler {
+
+    //@ExceptionHandler -> Anotação que mapeia exceções específicas para métodos manipuladores
+    //dentro de controllers ou conselheiros globais.
+    @ExceptionHandler(EntidadeNaoEncontradaException.class)
+    public ResponseEntity<?> tratarEntidadeNaoEncontradoException(EntidadeNaoEncontradaException e) {
+        Problema problema = Problema.builder()
+                .dataHora(LocalDateTime.now())
+                .mensagem(e.getMessage())
+                .build();
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(problema);
+    }
+
+    @ExceptionHandler(NegocioException.class)
+    public ResponseEntity<?> tratarNegocioException(NegocioException e) {
+        Problema problema = Problema.builder()
+                .dataHora(LocalDateTime.now())
+                .mensagem(e.getMessage())
+                .build();
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(problema);
+    }
+
+    @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
+    public ResponseEntity<?> tratarHttpMediaTypeNotSupportedException() {
+        Problema problema = Problema.builder()
+                .dataHora(LocalDateTime.now())
+                .mensagem("O tipo de mídia não é aceito.")
+                .build();
+
+        return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
+                .body(problema);
+    }
+
+}
+
+
