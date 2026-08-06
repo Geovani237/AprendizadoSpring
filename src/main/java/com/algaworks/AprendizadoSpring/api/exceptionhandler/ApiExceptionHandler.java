eb9052e4e4b0e82cfdf0c83a4f6b32632fbd68e5
Geovani Carlos de Souza <geovani.souza@colaborador.embrapa.br>
2026-08-06 10:20:20 -0300

    Customizando exception handlers de ResponseEntityExceptionHandler

--- src/main/java/com/algaworks/AprendizadoSpring/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/algaworks/AprendizadoSpring/api/exceptionhandler/ApiExceptionHandler.java
@@ -6,6 +6,7 @@ import com.algaworks.AprendizadoSpring.domain.exception.NegocioException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -63,6 +64,17 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
 
         Problem problem = createProblemBuilder(status, problemType, ex.getMessage()).build();
+
+        return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        ProblemType problemType = ProblemType.MENSAGEM_INCOMPREENSIVEL;
+        String detail = "O corpo da requisição está inválido. Verifique erro de sintaxe.";
+
+        Problem problem = createProblemBuilder(status, problemType, detail).build();
 
         return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
     }
