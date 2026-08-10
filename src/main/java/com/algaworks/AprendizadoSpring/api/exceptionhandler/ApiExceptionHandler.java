0e7c3cb68050e62ac0dddbc6df190a86bae90302
Geovani237 <geovanicsouza7@gmail.com>
2026-08-10 15:54:49 -0300

    tratando exception de violação de constraints de validação

--- src/main/java/com/algaworks/AprendizadoSpring/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/algaworks/AprendizadoSpring/api/exceptionhandler/ApiExceptionHandler.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -132,6 +133,22 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
         return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
     }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        ProblemType problemType = ProblemType.DADOS_INVALIDOS;
+
+        Problem problem = createProblemBuilder(status, problemType, "Um ou mais campos estão inválidos. Faça o preenchimento correto e tente novamente.")
+                .userMessage("Um ou mais campos estão inválidos. Faça o preenchimento correto e" +
+                        "tente novamente.")
+                .timestamp(LocalDateTime.now())
+                .build();
+
+        return handleExceptionInternal(ex, problem, headers, status, request);
+    }
+
+    //            MethodArgumentNotValidException
 
 
     @Override
