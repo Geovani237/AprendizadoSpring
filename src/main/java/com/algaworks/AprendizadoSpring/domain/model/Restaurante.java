b327ca4fca1a042fb83cc0859ca98a52a721de3c
Geovani Carlos de Souza <geovani.souza@colaborador.embrapa.br>
2026-08-11 10:08:22 -0300

    Validando as associações de uma entidade em cascata

--- src/main/java/com/algaworks/AprendizadoSpring/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/AprendizadoSpring/domain/model/Restaurante.java
@@ -8,6 +8,7 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
+import javax.validation.Valid;
 import javax.validation.constraints.*;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -38,6 +39,8 @@ public class Restaurante {
     //Many restaurantes podem pertencer a One cozinha
 //    @JsonIgnoreProperties("hibernateLazyInitializer")
 //    @JsonIgnore
+    @Valid
+    @NotNull
     @ManyToOne//(fetch = FetchType.LAZY)//Todas as associações ...ToOne usa Eager Loading, que seria um carregamento ansioso/antecipado
     @JoinColumn(name = "cozinha_id", nullable = false)
     private Cozinha cozinha;
