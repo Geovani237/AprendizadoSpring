4ca4884cf0402fd2b893e86ab1bc4edd9022761a
Geovani Carlos de Souza <geovani.souza@colaborador.embrapa.br>
2026-08-13 15:22:36 -0300

    Voltando o estado inicial do banco de dados para cada execução de teste com callback do Flyway

--- src/test/java/com/algaworks/AprendizadoSpring/CadastroCozinhaIT.java
+++ src/test/java/com/algaworks/AprendizadoSpring/CadastroCozinhaIT.java
@@ -1,10 +1,13 @@
 package com.algaworks.AprendizadoSpring;
 
+import com.algaworks.AprendizadoSpring.domain.model.Cozinha;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import org.flywaydb.core.Flyway;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
@@ -16,13 +19,16 @@ class CadastroCozinhaIT {
 	@LocalServerPort
 	private int port;
 
+	@Autowired
+	private Flyway flyway;
+
 	@BeforeEach
 	public void setUp() {
 		//Ajuda a debugar
 		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
 		RestAssured.port = port;
 		RestAssured.basePath = "/cozinhas";
-
+		flyway.migrate();
 	}
 
 	@Test
@@ -39,8 +45,6 @@ class CadastroCozinhaIT {
 
 	@Test
 	public void deveConter4Cozinhas_QuandoCunsultarCozinhas() {
-		//Ajuda a debugar
-
 		RestAssured.given()
 					.accept(ContentType.JSON)
 				.when()
@@ -51,4 +55,21 @@ class CadastroCozinhaIT {
 
 	}
 
+	@Test
+	public void deveRetornarStatus201_QuandoCadastrarCozinha() {
+		Cozinha cozinha = new Cozinha();
+		cozinha.setNome("Chinesa");
+
+		RestAssured.given()
+				.body(cozinha)
+				.contentType(ContentType.JSON)
+				.accept(ContentType.JSON)
+				.when()
+					.post()
+				.then()
+					.statusCode(HttpStatus.CREATED.value());
+	}
+
 }
+
+// Um teste não pode depender da execução de outro teste e nem na não execução de um outro teste
