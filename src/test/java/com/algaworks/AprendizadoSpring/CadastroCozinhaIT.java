2d7fb42db560897a07f95449538f10c5032bde18
Geovani Carlos de Souza <geovani.souza@colaborador.embrapa.br>
2026-08-13 14:53:42 -0300

    Validando o corpo da resposta HTTP

--- src/test/java/com/algaworks/AprendizadoSpring/CadastroCozinhaIT.java
+++ src/test/java/com/algaworks/AprendizadoSpring/CadastroCozinhaIT.java
@@ -1,19 +1,12 @@
 package com.algaworks.AprendizadoSpring;
 
-import com.algaworks.AprendizadoSpring.domain.exception.CozinhaNaoEncontradaException;
-import com.algaworks.AprendizadoSpring.domain.exception.EntidadeEmUsoException;
-import com.algaworks.AprendizadoSpring.domain.model.Cozinha;
-import com.algaworks.AprendizadoSpring.domain.service.CadastroCozinhaService;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
-import org.junit.jupiter.api.Assertions;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
-
-import javax.validation.ConstraintViolationException;
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -35,6 +28,23 @@ class CadastroCozinhaIT {
 				.get()
 			.then()
 				.statusCode(HttpStatus.OK.value());
+	}
+
+	@Test
+	public void deveConter4Cozinhas_QuandoCunsultarCozinhas() {
+		//Ajuda a debugar
+		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+
+		RestAssured.given()
+					.basePath("/cozinhas")
+					.port(pont)
+					.accept(ContentType.JSON)
+				.when()
+					.get()
+				.then()
+					.body("", Matchers.hasSize(4))
+				.body("nome", Matchers.hasItems("Indiana", "Tailandesa"));
+
 	}
 
 }
