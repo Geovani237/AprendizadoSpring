8fb118f6f436fe4ed4ca567502ce0fd1c1c21cea
Geovani237 <geovanicsouza7@gmail.com>
2026-08-14 17:15:58 -0300

    refatorando o código de testes

--- src/test/java/com/algaworks/AprendizadoSpring/CadastroCozinhaIT.java
+++ src/test/java/com/algaworks/AprendizadoSpring/CadastroCozinhaIT.java
@@ -2,6 +2,8 @@ package com.algaworks.AprendizadoSpring;
 
 import com.algaworks.AprendizadoSpring.domain.model.Cozinha;
 import com.algaworks.AprendizadoSpring.domain.repository.CozinhaRepository;
+import com.algaworks.AprendizadoSpring.util.DatabaseCleaner;
+import com.algaworks.AprendizadoSpring.util.ResourceUtil;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.hamcrest.Matchers;
@@ -12,12 +14,22 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.TestPropertySource;
-import com.algaworks.AprendizadoSpring.util.DatabaseCleaner;
+import org.springframework.util.ResourceUtils;
+import org.springframework.util.StreamUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestPropertySource("/application-test.properties")
 class CadastroCozinhaIT {
+
+	private Cozinha cozinhaAmericana;
+	private int quantidadeCozinhasCadastradas;
+	private String jsonCozinhaChinesa = "/json/cozinha-chinesa.json";
+	private static final Integer COZINHA_ID_INEXISTENTE = 100;
 
 	@LocalServerPort
 	private int port;
@@ -51,22 +63,19 @@ class CadastroCozinhaIT {
 	}
 
 	@Test
-	public void deveConter2Cozinhas_QuandoCunsultarCozinhas() {
+	public void deveHaverCozinhas_QuandoCunsultarCozinhas() {
 		RestAssured.given()
 					.accept(ContentType.JSON)
 				.when()
 					.get()
 				.then()
-					.body("", Matchers.hasSize(2));
+					.body("", Matchers.hasSize(quantidadeCozinhasCadastradas));
 	}
 
 	@Test
 	public void deveRetornarStatus201_QuandoCadastrarCozinha() {
-		Cozinha cozinha = new Cozinha();
-		cozinha.setNome("Chinesa");
-
 		RestAssured.given()
-				.body(cozinha)
+				.body(ResourceUtil.getContentFromResource(jsonCozinhaChinesa))
 				.contentType(ContentType.JSON)
 				.accept(ContentType.JSON)
 				.when()
@@ -79,20 +88,20 @@ class CadastroCozinhaIT {
 	public void deveRetornarRespostaEStatusCorretos_QuandoCulsultarCozinhaExistente() {
 		RestAssured
 				.given()
-					.pathParam("cozinhaId", 2)
+					.pathParam("cozinhaId", cozinhaAmericana.getId())
 					.accept(ContentType.JSON)
 				.when()
 					.get("/{cozinhaId}")
 				.then()
 				.statusCode(HttpStatus.OK.value())
-				.body("nome", Matchers.equalTo("Americana"));
+				.body("nome", Matchers.equalTo(cozinhaAmericana.getNome()));
 	}
 
 	@Test
 	public void deveRetornarRespostaEStatus404_QuandoCulsultarCozinhaInexistente() {
 		RestAssured
 				.given()
-					.pathParam("cozinhaId", 100)
+					.pathParam("cozinhaId", COZINHA_ID_INEXISTENTE)
 					.accept(ContentType.JSON)
 				.when()
 					.get("/{cozinhaId}")
@@ -105,9 +114,21 @@ class CadastroCozinhaIT {
 		cozinha1.setNome("Tailandesa");
 		cozinhaRepository.save(cozinha1);
 
-		Cozinha cozinha2 = new Cozinha();
-		cozinha2.setNome("Americana");
-		cozinhaRepository.save(cozinha2);
+		this.cozinhaAmericana = new Cozinha();
+		cozinhaAmericana.setNome("Americana");
+		cozinhaRepository.save(cozinhaAmericana);
+
+		quantidadeCozinhasCadastradas = (int) cozinhaRepository.count();
+
+	}
+
+	public static String getContentFromResource(String resourceName) {
+		try {
+			InputStream stream = ResourceUtils.class.getResourceAsStream(resourceName);
+			return StreamUtils.copyToString(stream, Charset.forName("UTF-8"));
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 }
---
+++ src/test/java/com/algaworks/AprendizadoSpring/util/ResourceUtil.java
@@ -0,0 +1,19 @@
+package com.algaworks.AprendizadoSpring.util;
+
+import org.springframework.util.StreamUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+
+public class ResourceUtil {
+
+    public static String getContentFromResource(String resourceName) {
+        try {
+            InputStream stream = ResourceUtil.class.getResourceAsStream(resourceName);
+            return StreamUtils.copyToString(stream, Charset.forName("UTF-8"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
