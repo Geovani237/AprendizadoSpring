ca6dbe8139302f7c79ed55b7465c8a3fdfc02f30
Geovani Carlos de Souza <geovani.souza@colaborador.embrapa.br>
2026-08-18 09:25:39 -0300

    Refinando o payload de cadastro com @JsonIgnoreProperties

--- src/main/java/com/algaworks/AprendizadoSpring/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/AprendizadoSpring/domain/model/Restaurante.java
@@ -2,6 +2,7 @@ package com.algaworks.AprendizadoSpring.domain.model;
 
 import com.algaworks.AprendizadoSpring.core.validation.Groups;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.hibernate.annotations.CreationTimestamp;
@@ -48,6 +49,7 @@ public class Restaurante {
     //Many restaurantes podem pertencer a One cozinha
 //    @JsonIgnoreProperties("hibernateLazyInitializer")
 //    @JsonIgnore
+    @JsonIgnoreProperties(value = "nome", allowGetters = true)
     @Valid
     @ConvertGroup(from = Default.class, to = Groups.CozinhaId.class)
     @NotNull
