55e63d323c1f852e6e4834693f24d27f902bfabd
Geovani Carlos de Souza <geovani.souza@colaborador.embrapa.br>
2026-08-19 10:25:13 -0300

    Adicionando e usando o ModelMapper

--- src/main/java/com/algaworks/AprendizadoSpring/api/disassembler/RestauranteInputDisassembler.java
+++ src/main/java/com/algaworks/AprendizadoSpring/api/disassembler/RestauranteInputDisassembler.java
@@ -3,20 +3,27 @@ package com.algaworks.AprendizadoSpring.api.disassembler;
 import com.algaworks.AprendizadoSpring.api.model.input.RestauranteInput;
 import com.algaworks.AprendizadoSpring.domain.model.Cozinha;
 import com.algaworks.AprendizadoSpring.domain.model.Restaurante;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RestauranteInputDisassembler {
 
+    @Autowired
+    private ModelMapper modelMapper;
+
     public Restaurante toDomainObject(RestauranteInput restauranteInput) {
-        Cozinha cozinha = new Cozinha();
-        cozinha.setId(restauranteInput.getCozinha().getId());;
 
-        Restaurante restaurante = new Restaurante();
-        restaurante.setNome(restauranteInput.getNome());
-        restaurante.setTaxaFrete(restauranteInput.getTaxaFrete());
-        restaurante.setCozinha(cozinha);
-
-        return restaurante;
+        return modelMapper.map(restauranteInput, Restaurante.class);
+//        Cozinha cozinha = new Cozinha();
+//        cozinha.setId(restauranteInput.getCozinha().getId());;
+//
+//        Restaurante restaurante = new Restaurante();
+//        restaurante.setNome(restauranteInput.getNome());
+//        restaurante.setTaxaFrete(restauranteInput.getTaxaFrete());
+//        restaurante.setCozinha(cozinha);
+//
+//        return restaurante;
     }
 }
