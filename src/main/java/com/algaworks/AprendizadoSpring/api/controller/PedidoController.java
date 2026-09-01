6f5bc497ae11c87db30bebac42788898f1661fe6
Geovani Carlos de Souza <geovani.souza@colaborador.embrapa.br>
2026-09-01 14:45:55 -0300

    Limitando os campos retornados pela API com Squiggly

--- src/main/java/com/algaworks/AprendizadoSpring/api/controller/PedidoController.java
+++ src/main/java/com/algaworks/AprendizadoSpring/api/controller/PedidoController.java
@@ -42,29 +42,29 @@ public class PedidoController {
     @Autowired
     private EmissaoPedidoService cadastroPedido;
 
-    @GetMapping
-    public MappingJacksonValue listar(@RequestParam(required = false) String campos) {
-        List<Pedido> pedidos = pedidoRepository.findAll();
-        List<PedidoResumoModel> pedidosModel = pedidoResumoModelAssembler.toCollectionModel(pedidos);
-
-        MappingJacksonValue pedidosWrapper = new MappingJacksonValue(pedidosModel);
-
-        SimpleFilterProvider filterProvider = new SimpleFilterProvider();
-        filterProvider.addFilter("pedidoFilter", SimpleBeanPropertyFilter.serializeAll());
-
-        if (StringUtils.isNotBlank(campos)) {
-            filterProvider.addFilter("pedidoFilter", SimpleBeanPropertyFilter.filterOutAllExcept(campos.split(",")));
-        }
-
-        pedidosWrapper.setFilters(filterProvider);
-
-        return pedidosWrapper;
-    }
-
 //    @GetMapping
-//    public List<PedidoResumoModel> listar() {
-//        return pedidoResumoModelAssembler.toCollectionModel(pedidoRepository.findAll());
+//    public MappingJacksonValue listar(@RequestParam(required = false) String campos) {
+//        List<Pedido> pedidos = pedidoRepository.findAll();
+//        List<PedidoResumoModel> pedidosModel = pedidoResumoModelAssembler.toCollectionModel(pedidos);
+//
+//        MappingJacksonValue pedidosWrapper = new MappingJacksonValue(pedidosModel);
+//
+//        SimpleFilterProvider filterProvider = new SimpleFilterProvider();
+//        filterProvider.addFilter("pedidoFilter", SimpleBeanPropertyFilter.serializeAll());
+//
+//        if (StringUtils.isNotBlank(campos)) {
+//            filterProvider.addFilter("pedidoFilter", SimpleBeanPropertyFilter.filterOutAllExcept(campos.split(",")));
+//        }
+//
+//        pedidosWrapper.setFilters(filterProvider);
+//
+//        return pedidosWrapper;
 //    }
+
+    @GetMapping
+    public List<PedidoResumoModel> listar() {
+        return pedidoResumoModelAssembler.toCollectionModel(pedidoRepository.findAll());
+    }
 
     @GetMapping("/{codigoId}")
     public PedidoModel buscar(@PathVariable String codigoId) {
