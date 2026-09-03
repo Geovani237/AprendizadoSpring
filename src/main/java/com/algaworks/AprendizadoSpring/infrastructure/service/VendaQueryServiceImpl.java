01d01d9247767ebe6a79172a7dd8653a6e15c0a5
Geovani Carlos de Souza <geovani.souza@colaborador.embrapa.br>
2026-09-03 15:25:34 -0300

    Implementando consulta com dados agregados de vendas diárias

--- src/main/java/com/algaworks/AprendizadoSpring/infrastructure/service/VendaQueryServiceImpl.java
+++ src/main/java/com/algaworks/AprendizadoSpring/infrastructure/service/VendaQueryServiceImpl.java
@@ -1,20 +1,41 @@
 package com.algaworks.AprendizadoSpring.infrastructure.service;
 
 import com.algaworks.AprendizadoSpring.domain.filter.VendaDiariaFilter;
+import com.algaworks.AprendizadoSpring.domain.model.Pedido;
 import com.algaworks.AprendizadoSpring.domain.model.dto.VendaDiaria;
 import com.algaworks.AprendizadoSpring.domain.service.VendaQueryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import java.util.Date;
 import java.util.List;
 
+@Repository
 public class VendaQueryServiceImpl implements VendaQueryService {
 
     @Autowired
-    private EntityManager entityManager;
+    private EntityManager manager;
 
     @Override
     public List<VendaDiaria> consultarVendasDiarias(VendaDiariaFilter filter) {
-        return List.of();
+
+        var builder = manager.getCriteriaBuilder();
+        var query = builder.createQuery(VendaDiaria.class);
+        var root = query.from(Pedido.class);
+
+        //Seria o Date do MySQL que é semelhante ao LocalDate
+        var functionDateDataCriacao = builder.function(
+                "date", Date.class, root.get("dataCriacao"));
+
+        var selection = builder.construct(VendaDiaria.class,
+                functionDateDataCriacao,//data
+                builder.count(root.get("id")),//id do pedido
+                builder.sum(root.get("valorTotal")));
+
+        query.select(selection);
+        query.groupBy(functionDateDataCriacao);
+
+        return manager.createQuery(query).getResultList();
     }
 }
