68de1af26c7d74205db778f2c2c873e6da4e2d30
Geovani Carlos de Souza <geovani.souza@colaborador.embrapa.br>
2026-09-03 16:28:49 -0300

    adicionando os filtros na consulta de vendas diárias

--- src/main/java/com/algaworks/AprendizadoSpring/infrastructure/service/VendaQueryServiceImpl.java
+++ src/main/java/com/algaworks/AprendizadoSpring/infrastructure/service/VendaQueryServiceImpl.java
@@ -2,17 +2,21 @@ package com.algaworks.AprendizadoSpring.infrastructure.service;
 
 import com.algaworks.AprendizadoSpring.domain.filter.VendaDiariaFilter;
 import com.algaworks.AprendizadoSpring.domain.model.Pedido;
+import com.algaworks.AprendizadoSpring.domain.model.StatusPedido;
 import com.algaworks.AprendizadoSpring.domain.model.dto.VendaDiaria;
 import com.algaworks.AprendizadoSpring.domain.service.VendaQueryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import javax.persistence.criteria.Predicate;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 @Repository
-public class VendaQueryServiceImpl implements VendaQueryService {
+public class VendaQueryServiceImpl implements VendaQueryService  {
 
     @Autowired
     private EntityManager manager;
@@ -24,6 +28,8 @@ public class VendaQueryServiceImpl implements VendaQueryService {
         var query = builder.createQuery(VendaDiaria.class);
         var root = query.from(Pedido.class);
 
+        var predicates = new ArrayList<Predicate>();
+
         //Seria o Date do MySQL que é semelhante ao LocalDate
         var functionDateDataCriacao = builder.function(
                 "date", Date.class, root.get("dataCriacao"));
@@ -33,6 +39,23 @@ public class VendaQueryServiceImpl implements VendaQueryService {
                 builder.count(root.get("id")),//id do pedido
                 builder.sum(root.get("valorTotal")));
 
+        if (filter.getRestauranteId() != null) {
+            predicates.add(builder.equal(root.get("restaurante").get("id"), filter.getRestauranteId()));
+        }
+
+        if (filter.getDataCriacaoInicio() != null) {
+            predicates.add(builder.greaterThanOrEqualTo(root.get("dataCriacao"), filter.getDataCriacaoInicio()));
+        }
+
+        if (filter.getDataCriacaoFim() != null) {
+            predicates.add(builder.lessThanOrEqualTo(root.get("dataCriacao"), filter.getDataCriacaoFim()));
+        }
+
+        predicates.add(root.get("status").in(
+                StatusPedido.CONFIRMADO, StatusPedido.ENTREGUE
+        ));
+
+        query.where(predicates.toArray(new Predicate[0]));
         query.select(selection);
         query.groupBy(functionDateDataCriacao);
 
