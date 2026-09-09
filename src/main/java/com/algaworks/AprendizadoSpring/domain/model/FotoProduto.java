5e8746ed170b2e5c253fef26141a78dce8537776
Geovani Carlos de Souza <geovani.souza@colaborador.embrapa.br>
2026-09-09 09:22:45 -0300

    Mapeando entidade FotoProduto e relacionamento um-para-um

---
+++ src/main/java/com/algaworks/AprendizadoSpring/domain/model/FotoProduto.java
@@ -0,0 +1,30 @@
+package com.algaworks.AprendizadoSpring.domain.model;
+
+import com.algaworks.AprendizadoSpring.core.validation.Groups;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Data
+@Entity
+public class FotoProduto {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @Column(name = "produto_id")
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @MapsId//Essa anotação diz que o atributo produto é mapeado através do Id da entidade FotoProduto
+    private Produto produto;
+
+    private String nomeArquivo;
+    private String descricao;
+    private String contentType;
+    private Long tamanho;
+
+
+}
