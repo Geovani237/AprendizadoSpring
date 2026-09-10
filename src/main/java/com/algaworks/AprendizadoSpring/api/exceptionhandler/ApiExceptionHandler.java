4f2c6568a852f585a3941a8e02068db10038fa82
Geovani Carlos de Souza <geovani.souza@colaborador.embrapa.br>
2026-09-10 11:44:47 -0300

    Corrigindo exception handler de media type não aceita

--- src/main/java/com/algaworks/AprendizadoSpring/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/algaworks/AprendizadoSpring/api/exceptionhandler/ApiExceptionHandler.java
@@ -18,6 +18,7 @@ import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -141,6 +142,12 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
     public ResponseEntity<Object> handleValidacaoException(ValidacaoException ex, WebRequest request) {
         return handleValidationInternal(ex, ex.getBindingResult(), new HttpHeaders(),
                 HttpStatus.BAD_REQUEST, request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMediaTypeNotAcceptable(HttpMediaTypeNotAcceptableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        return ResponseEntity.status(status).headers(headers).build();
     }
 
     @Override
