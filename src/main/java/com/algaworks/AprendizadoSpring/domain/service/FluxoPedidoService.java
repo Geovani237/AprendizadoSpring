7d95f65773ae444cd9a2016403c20e0c3e1ccfb8
Geovani237 <geovanicsouza7@gmail.com>
2026-09-15 22:10:27 -0300

    testes com S3 no localStack

--- src/main/java/com/algaworks/AprendizadoSpring/domain/service/FluxoPedidoService.java
+++ src/main/java/com/algaworks/AprendizadoSpring/domain/service/FluxoPedidoService.java
@@ -14,22 +14,22 @@ public class FluxoPedidoService {
 
     @Autowired
     private EmissaoPedidoService emissaoPedido;
-
-    @Autowired
-    private EnvioEmailService envioEmail;
+//
+//    @Autowired
+//    private EnvioEmailService envioEmail;
 
     @Transactional
     public void confirmar(String codigoPedido) {
         Pedido pedido = emissaoPedido.buscarOuFalhar(codigoPedido);
         pedido.confirmar();
 
-        var mensagem = EnvioEmailService.Mensagem.builder()
-                .assunto(pedido.getRestaurante().getNome() + " - Pedido confirmado")
-                .corpo("O pedido de código <strong>" + pedido.getCodigo() + "</strong> foi confirmado")
-                .destinatario(pedido.getCliente().getEmail())
-                .build();
-
-        envioEmail.enviar(mensagem);
+//        var mensagem = EnvioEmailService.Mensagem.builder()
+//                .assunto(pedido.getRestaurante().getNome() + " - Pedido confirmado")
+//                .corpo("O pedido de código <strong>" + pedido.getCodigo() + "</strong> foi confirmado")
+//                .destinatario(pedido.getCliente().getEmail())
+//                .build();
+//
+//        envioEmail.enviar(mensagem);
     }
 
     @Transactional
