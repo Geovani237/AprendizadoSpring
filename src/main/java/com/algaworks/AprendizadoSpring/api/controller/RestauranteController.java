ab1d7853d3ccaad1d82513f3feb52fededcb0070
Geovani Carlos de Souza <geovani.souza@colaborador.embrapa.br>
2026-09-17 13:43:12 -0300

    Entendendo o funcionamento básico de CORS e habilitando na API

--- src/main/java/com/algaworks/AprendizadoSpring/api/controller/RestauranteController.java
+++ src/main/java/com/algaworks/AprendizadoSpring/api/controller/RestauranteController.java
@@ -12,8 +12,10 @@ import com.algaworks.AprendizadoSpring.domain.model.Restaurante;
 import com.algaworks.AprendizadoSpring.domain.repository.RestauranteRepository;
 import com.algaworks.AprendizadoSpring.domain.service.CadastroRestauranteService;
 import com.fasterxml.jackson.annotation.JsonView;
+import com.google.common.net.HttpHeaders;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.validation.SmartValidator;
 import org.springframework.web.bind.annotation.*;
@@ -39,15 +41,19 @@ public class RestauranteController {
 
     @JsonView(RestauranteView.Resumo.class)
     @GetMapping
-    public List<RestauranteModel> listar() {
-        return restauranteModelAssembler.toCollectionModel(restauranteRepository.findAll());
+    public ResponseEntity<List<RestauranteModel>> listar() {
+        List<RestauranteModel> restauranteModel =  restauranteModelAssembler.toCollectionModel(restauranteRepository.findAll());
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "*")
+                .body(restauranteModel);
     }
 
-    @JsonView(RestauranteView.ApenasNome.class)
-    @GetMapping(params = "projecao=apenas-nome")
-    public List<RestauranteModel> listarResumido() {
-        return listar();
-    }
+//    @JsonView(RestauranteView.ApenasNome.class)
+//    @GetMapping(params = "projecao=apenas-nome")
+//    public List<RestauranteModel> listarResumido() {
+//        return listar();
+//    }
 
 
 //    @GetMapping
