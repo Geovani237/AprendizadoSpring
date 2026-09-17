eb403b6459f4dde7a3d1400a8f86afe7f2a29784
Geovani Carlos de Souza <geovani.souza@colaborador.embrapa.br>
2026-09-17 14:23:52 -0300

    Habilitando CORS globalmente no projeto da API

--- src/main/java/com/algaworks/AprendizadoSpring/api/controller/RestauranteController.java
+++ src/main/java/com/algaworks/AprendizadoSpring/api/controller/RestauranteController.java
@@ -21,7 +21,6 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:8000")
 @RestController
 @RequestMapping(value = "/restaurantes")
 public class RestauranteController {
