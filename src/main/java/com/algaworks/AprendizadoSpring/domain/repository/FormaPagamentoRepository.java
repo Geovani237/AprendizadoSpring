08c7b1715afafe646914726048ba675fd8c8baac
Geovani Carlos de Souza <geovani.souza@colaborador.embrapa.br>
2026-09-22 11:56:22 -0300

    Entendendo e preparando a implementação de Deep ETags

--- src/main/java/com/algaworks/AprendizadoSpring/domain/repository/FormaPagamentoRepository.java
+++ src/main/java/com/algaworks/AprendizadoSpring/domain/repository/FormaPagamentoRepository.java
@@ -2,9 +2,14 @@ package com.algaworks.AprendizadoSpring.domain.repository;
 
 import com.algaworks.AprendizadoSpring.domain.model.FormaPagamento;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.time.OffsetDateTime;
 
 @Repository
 public interface FormaPagamentoRepository extends JpaRepository<FormaPagamento, Long> {
 
+    @Query("select max(dataAtualizacao) from FormaPagamento")
+    OffsetDateTime getUltimaAtualizacao();
 }
