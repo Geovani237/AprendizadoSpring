3b5a9f67976a46a91d5293ee7978e3085a3a5d47
Geovani Carlos de Souza <geovani.souza@colaborador.embrapa.br>
2026-09-23 10:24:01 -0300

    Corrigindo documentação com substituição de Pageable

--- src/main/java/com/algaworks/AprendizadoSpring/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/algaworks/AprendizadoSpring/core/openapi/SpringFoxConfig.java
@@ -1,11 +1,13 @@
 package com.algaworks.AprendizadoSpring.core.openapi;
 
 import com.algaworks.AprendizadoSpring.api.exceptionhandler.Problem;
+import com.algaworks.AprendizadoSpring.core.openapi.model.PageableModelOpenApi;
 import com.fasterxml.classmate.TypeResolver;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
@@ -43,6 +45,7 @@ public class SpringFoxConfig {
                 .globalResponses(HttpMethod.PUT, globalPostPutResponseMessages())
                 .globalResponses(HttpMethod.DELETE, globalDeleteResponseMessages())
                 .additionalModels(typeResolver.resolve(Problem.class))
+                .directModelSubstitute(Pageable.class, PageableModelOpenApi.class)
                 .apiInfo(apiInfo())
                 .tags(new Tag("Cidades", "Gerencia as cidades"))
                 .tags(new Tag("Grupos", "Gerencia os grupos de usuários"));
