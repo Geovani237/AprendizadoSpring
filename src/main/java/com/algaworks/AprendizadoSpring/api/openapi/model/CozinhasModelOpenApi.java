31e60c08712a46354bd1b09167e4db5d43b094a8
Geovani Carlos de Souza <geovani.souza@colaborador.embrapa.br>
2026-09-23 10:55:24 -0300

    criando um pageModelOpenApi generica

--- src/main/java/com/algaworks/AprendizadoSpring/api/openapi/model/CozinhasModelOpenApi.java
+++ src/main/java/com/algaworks/AprendizadoSpring/api/openapi/model/CozinhasModelOpenApi.java
@@ -2,29 +2,10 @@ package com.algaworks.AprendizadoSpring.api.openapi.model;
 
 import com.algaworks.AprendizadoSpring.api.model.CozinhaModel;
 import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
-import lombok.Getter;
-import lombok.Setter;
-
-import java.util.List;
 
 @ApiModel("CozinhasModel")
-@Getter
-@Setter
-public class CozinhasModelOpenApi {
+public class CozinhasModelOpenApi extends PageModelOpenApi<CozinhaModel>{
 
-    private List<CozinhaModel> content;
 
-    @ApiModelProperty(example = "10", value = "Quantidade de registros por página")
-    private Long size;
-
-    @ApiModelProperty(example = "50", value = "Total de registros")
-    private Long totalElements;
-
-    @ApiModelProperty(example = "5", value = "Total de páginas")
-    private Long totalPages;
-
-    @ApiModelProperty(example = "0", value = "Número da página (começa em 0) ")
-    private Long number;
 
 }
