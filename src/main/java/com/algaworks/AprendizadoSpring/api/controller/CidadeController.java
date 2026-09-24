7db34aa1a3ef190852d24f043a8206c19c32c750
Geovani Carlos de Souza <geovani.souza@colaborador.embrapa.br>
2026-09-24 09:15:55 -0300

    Adicionando hypermedia na representação de recurso único com HAL

--- src/main/java/com/algaworks/AprendizadoSpring/api/controller/CidadeController.java
+++ src/main/java/com/algaworks/AprendizadoSpring/api/controller/CidadeController.java
@@ -12,6 +12,8 @@ import com.algaworks.AprendizadoSpring.domain.model.Cidade;
 import com.algaworks.AprendizadoSpring.domain.repository.CidadeRepository;
 import com.algaworks.AprendizadoSpring.domain.service.CadastroCidadeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.IanaLinkRelations;
+import org.springframework.hateoas.Link;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -53,7 +55,17 @@ public class CidadeController implements CidadeControllerOpenApi {
     public CidadeModel buscar(@PathVariable Long cidadeId) {
         Cidade cidade = cadastroCidade.buscarOuFalhar(cidadeId);
 
-        return cidadeModelAssembler.toModel(cidade);
+        CidadeModel cidadeModel = cidadeModelAssembler.toModel(cidade);
+
+        cidadeModel.add(Link.of("localhost:8080/cidades/1"));
+//		cidadeModel.add(Link.of("localhost:8080/cidades/1", IanaLinkRelations.SELF));
+
+//		cidadeModel.add(Link.of("localhost:8080/cidades/1", IanaLinkRelations.COLLECTION));
+        cidadeModel.add(Link.of("localhost:8080/cidades/1", "cidades"));
+
+        cidadeModel.getEstado().add(Link.of("localhost:8080/estados/1"));
+
+        return cidadeModel;
     }
 
 
